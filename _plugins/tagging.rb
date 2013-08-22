6da036a9c5430702df963e34e7b148014d034c46
Raik Ilves <raik.ilves@gmail.com>
2013-08-23 00:07:59 +0300

    Remove ", " (comma) after tags

--- _plugins/tagging.rb
+++ _plugins/tagging.rb
@@ -112,7 +112,7 @@ module Jekyll
       tags = obj['tags'].dup
       tags.map! { |t| t.first } if tags.first.is_a?(Array)
       tags.map! { |t| tag_link(t, tag_url(t), :rel => 'tag') if t.is_a?(String) }.compact!
-      tags.join(', ')
+      tags.join(' ')
     end
 
     def active_tag_data(site = Tagger.site)
